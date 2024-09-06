a1a2a347e43456034e94cc704467c345803278fe
Nischal <thapanischal39@gmail.com>
2024-09-06 21:22:49 +0545

    Refactoring #7

---
+++ TaskDetails.vb
@@ -0,0 +1,11 @@
+﻿Public Class TaskDetails
+    Public Property TaskID As Integer
+    Public Property Title As String
+    Public Property EntryDateTime As String
+    Public Property IsImportant As Boolean
+    Public Property IsRepeated As Boolean
+    Public Property Description As String
+    Public Property Reminder As String
+    Public Property RepeatFrequency As String
+    Public Property DueDate As String
+End Class
